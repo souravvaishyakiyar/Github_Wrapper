e9f38ebf2057311f8b0f661f9ad520a41cc08b1e
souravvaishyakiyar <souravvaishyakiyar@gmail.com>
2023-10-27 00:54:02 +0530

    done

--- script.js
+++ script.js
@@ -4,6 +4,39 @@ const showDetailsButton=document.getElementById('showDetails')
 
 const profileInfoDiv = document.getElementById('profileInfo');
 
+const reposInfoDiv=document.getElementById('reposInfo');
+function showReposInfo(userName){
+    fetch(`https://api.github.com/users/${userName}/repos`)
+    .then(res =>res.json())
+    .then(projects =>{
+        console.log(projects)
+        for(let i=0;i<projects.length;i++)
+        {
+            reposInfoDiv.innerHTML+=`<div class="card">
+           
+            <div class="card-body">
+                <div class="card-title">${projects[i].name}</div>
+                <div class="class-subHeading">${projects[i].language}</div>
+    
+                <div class="card-text">
+                    
+                    <button>
+                    <a href=${projects[i].html_url}>Do checkout project
+                    </a>
+
+
+                    </button>
+                 
+                </div>
+                
+            </div>
+    
+        </div>`
+        }
+       
+    })
+
+}   
 
 showDetailsButton.addEventListener('click',()=>{
     const userName=userNameInput.value;
@@ -11,7 +44,7 @@ showDetailsButton.addEventListener('click',()=>{
     fetch(`https://api.github.com/users/${userName}`)
     .then((res)=>res.json())
     .then((data)=>{
-        console.log(data);
+       // console.log(data);
         profileInfoDiv.innerHTML=`<div class="card">
         <div class="card-img">
             <img src="${data.avatar_url}" alt="${data.name}">
@@ -24,10 +57,17 @@ showDetailsButton.addEventListener('click',()=>{
                 <p>${data.bio}</p>
                 <p>${data.followers} followers ${data.following} 
                 following</p>
+                <button>
+                <a href=${data.html_url}>Do checkout profile
+                </a>
+
+
+                </button>
             </div>
             
         </div>
 
     </div>`
+        showReposInfo(userName);
     })
 })
